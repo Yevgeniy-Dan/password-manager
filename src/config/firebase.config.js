c5d74e205d5098c07eec6066b8186ff1ca99c6e8
Yevhenii Danoi <geneforapp@gmail.com>
2023-02-01 11:28:53 +0200

    secure configuration

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQeaPUPpP_YLIqSfDiZWFAJaaTyogWvgw",
-  authDomain: "technical-tasks.firebaseapp.com",
-  projectId: "technical-tasks",
-  storageBucket: "technical-tasks.appspot.com",
-  messagingSenderId: "585064261760",
-  appId: "1:585064261760:web:22235e3766a780a9571e65",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 export const app = initializeApp(firebaseConfig);
 
